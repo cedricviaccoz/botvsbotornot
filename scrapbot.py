960569c3d27c54e33ebe6eba62e48a86d946b254
cedricviaccoz <cedric.viaccoz@gmail.com>
2018-03-24 16:55:05 +0100

    Empty JSON dumps + first automating click steps

--- scrapbot.py
+++ scrapbot.py
@@ -1,20 +1,39 @@
-import BeautifulSoup as soup 
-import os
-import path
+#!/usr/bin/env python
+from bs4 import BeautifulSoup 
+import sys
+from os import path
 import json
+from selenium import webdriver
 
-SCRAP_URL = "http://botpoet.com/vote/random/"
+SCRAP_URL = "http://botpoet.com/"
 
-args = os.argv
+args = sys.argv
 
 def main(file):
-	pass
+	browser = webdriver.Chrome()
+	browser.get(SCRAP_URL)
+	startLine = browser.find_element_by_link_text("Free play")
+	startLine.click()
+	itsAbot = browser.find_element_by_link_text("Bot")
+	itsABot.clic()
+	currUrl = browser.current_url
+
+	duplicateThreshold = 100
+	duplcates = 0
+	page = BeautifulSoup(urllib2.urlopen(SCRAP_URL))
+
 
 
 
 if len(args) < 2:
-	print("please specify filename where to store poems in line command argument.")
+	print("please specify filename where to store poems in first line command argument.")
 else:
 	if not path.exists(args[1]):
-		pass
-		#TODO create the file with empty json file structure.
+		with open(args[1], 'w') as f:
+			#initializing empty file with correct structure.
+			toDump = json.dumps(
+				{"titles":[],
+				 "poems":[]
+				})
+			f.write(toDump)
+	main(args[1])
